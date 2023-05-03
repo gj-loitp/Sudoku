8fe2fb5637bcefc825aa5ecd16f1822879ecab47
loitp <www.muathu@gmail.com>
2023-05-03 14:43:05 +0700

    up

--- app/src/main/java/com/sudoku/MainActivity.kt
+++ app/src/main/java/com/sudoku/MainActivity.kt
@@ -4,23 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentTransaction
 import com.roy.itf.Communicator
+import com.sudoku.databinding.AMainBinding
 
 class MainActivity : AppCompatActivity(), Communicator {
 
+    private lateinit var binding: AMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.a_main)
 
+        binding = AMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     override fun passDataCom(input: String) {
         val bundle = Bundle()
-        bundle.putString("difficulty_text",input)
-
+        bundle.putString("difficulty_text", input)
         val transaction = this.supportFragmentManager.beginTransaction()
         val playFrag = PlayScreen()
         playFrag.arguments = bundle
-
         transaction.replace(R.id.mainActivityId, playFrag)
         transaction.addToBackStack(null)
         transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
