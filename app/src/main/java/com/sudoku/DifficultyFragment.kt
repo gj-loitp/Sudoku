1b67381dfc5f3076486cdcee8d21f3d6b60323eb
loitp <www.muathu@gmail.com>
2023-05-03 14:39:13 +0700

    up

--- app/src/main/java/com/sudoku/DifficultyFragment.kt
+++ app/src/main/java/com/sudoku/DifficultyFragment.kt
@@ -8,37 +8,36 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.findNavController
 import com.roy.itf.Communicator
+import com.sudoku.databinding.FDifficultyBinding
 
 class DifficultyFragment : Fragment() {
-
+    private lateinit var binding: FDifficultyBinding
     lateinit var comm: Communicator
 
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val view: View = inflater.inflate(R.layout.f_difficulty, container, false)
-        return view
+        binding = FDifficultyBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val easyButton: Button = view.findViewById(R.id.btEasy)
-        val mediumButton: Button = view.findViewById(R.id.btMedium)
-        val hardButton: Button = view.findViewById(R.id.btHard)
-
         comm = activity as Communicator
 
-        easyButton.setOnClickListener {
+        binding.btEasy.setOnClickListener {
             comm.passDataCom("easy")
             view.findNavController().navigate(R.id.action_difficultyFragment_to_playScreen)
         }
-        mediumButton.setOnClickListener {
+        binding.btMedium.setOnClickListener {
             comm.passDataCom("medium")
             view.findNavController().navigate(R.id.action_difficultyFragment_to_playScreen)
         }
-        hardButton.setOnClickListener {
+        binding.btHard.setOnClickListener {
             comm.passDataCom("hard")
             view.findNavController().navigate(R.id.action_difficultyFragment_to_playScreen)
         }
