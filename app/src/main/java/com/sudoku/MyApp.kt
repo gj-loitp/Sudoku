e3f8b85599ab992c4d2ded2228ae0c9992c8a1e6
loitp <www.muathu@gmail.com>
2023-05-03 13:03:02 +0700

    up

---
+++ app/src/main/java/com/sudoku/MyApp.kt
@@ -0,0 +1,18 @@
+package com.sudoku
+
+import android.app.Application
+
+//TODO ic_launcher
+//TODO rate app
+//TODO more app
+//TODO share app
+//TODO policy
+//TODO firebase
+//TODO ad
+//TODO keystore
+//TODO proguard
+class MyApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
